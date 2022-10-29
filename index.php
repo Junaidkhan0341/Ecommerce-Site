a7a8ab223e832aa4ac327e2f185bc2d9fe1e467b
Junaidkhan0341 <junaid@dotsnpix.com>
2022-10-29 23:16:15 +0500

    First-one

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <div id="top">
+
+    <div class="container">
+
+    <div class="col-md-6 offer">
+
+    <a href="#" class="btn btn-success btn-sm">Welcome</a>
+    <a href="checkout.php">4 Items in your Cart | Shopping Cart Total Price: $300</a>
+
+    </div>
+
+    <div class="col-md-6">
+
+    <ul class="menu">
+        <li><a href="customer-register.php">Register</a></li>
+        <li><a href="checkout.php">My Account</a></li>
+        <li><a href="cart.php">Go To Cart</a></li>
+        <li><a href="checkout.php">Login</a></li>
+    </ul>
+    </div>
+
+    </div>
+
+    </div>
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
 </body>
+
 </html>
