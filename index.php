5cd89c75266c8dfbc3079f5f5deb19b4573b37b8
Junaidkhan0341 <junaid@dotsnpix.com>
2022-11-05 11:02:05 +0500

    slider

--- index.php
+++ index.php
@@ -106,6 +106,42 @@
         </div>
     </div>
 
+    <div class="container" id="slider">
+        <div class="col-md-12">
+            <div class="carousel slide" id="myCarousel" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="admin_area/admin_images/1.jpg" alt="Slider Image 1">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/admin_images/2.jpg" alt="Slider Image 2">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/admin_images/3.jpg" alt="Slider Image 3">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/admin_images/4.jpg" alt="Slider Image 4">
+                    </div>
+                </div>
+                <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                    <span class="fa fa-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a href="#myCarousel" class="right carousel-control" data-slide="next">
+                    <span class="fa fa-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
